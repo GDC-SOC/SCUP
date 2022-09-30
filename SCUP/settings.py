12eff46aebfea29b1ff55980e656d638c73cea7c
Joseph Volosin <josephvolosin@gmail.com>
2022-09-30 12:17:25 -0400

    Initial commit of the homepage

--- SCUP/settings.py
+++ SCUP/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'SCUP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            Path(BASE_DIR) / "home" / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    str(BASE_DIR / "home" / "static"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
