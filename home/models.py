9080c045c9bb88a9e18658c1f5a3a79618aa45df
Joseph Volosin <josephvolosin@gmail.com>
2022-10-07 10:42:09 -0400

    Adding simple model for the SCUP homepage bio

--- home/models.py
+++ home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class SCUPBio(models.Model):
+    """Model for the homepage bio content"""
+    content = models.TextField()
--- home/views.py
+++ home/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-
-SCUP_HOMEPAGE_TOP_BIO = """
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis efficitur nunc, sed tincidunt ligula efficitur sed. Duis a dolor sit amet enim venenatis pulvinar. Nunc semper pretium leo, eget aliquet lorem fringilla ac. Curabitur euismod vel erat et lobortis. Nulla varius varius orci, eu venenatis massa ullamcorper ut. Cras quam est, tristique sed tempus sit amet, scelerisque at justo. Maecenas eleifend nibh sodales, consequat tellus eu, cursus quam. Morbi sit amet accumsan ipsum, eget tempus nulla. 
-"""
+from .models import SCUPBio
 
 # Create your views here.
 def index(request):
     """Handles the main SCUP homepage"""
-    return render(request, 'home/index.html', {"top_bio": SCUP_HOMEPAGE_TOP_BIO})
+    return render(request, 'home/index.html', {"top_bio": SCUPBio.objects.all().first().content})
