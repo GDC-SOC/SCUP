c30ba58fef878740666a6cb2f0de15852ded38af
Joseph <josephvolosin@gmail.com>
2022-10-18 09:26:09 -0400

    Fixing typo of SCUPBIO -> SCUPBio

--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from .models import SCUPBio, MoSAICBio, AETHERBio, CAPEBio
 # Create your views here.
 def index(request):
     """Handles the main SCUP homepage"""
-    scup_bio = SCUPBIO.objects.all().first().content if SCUPBio.objects.all().first() else "Work in progress!"
+    scup_bio = SCUPBio.objects.all().first().content if SCUPBio.objects.all().first() else "Work in progress!"
     mosaic_bio = MoSAICBio.objects.all().first().content if MoSAICBio.objects.all().first() else "Work in progress!"
     aether_bio = AETHERBio.objects.all().first().content if AETHERBio.objects.all().first() else "Work in progress!"
     cape_bio = CAPEBio.objects.all().first().content if CAPEBio.objects.all().first() else "Work in progress!"
