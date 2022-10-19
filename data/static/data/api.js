a401f2ad97fb2762923dddfe17a5934ef90e7d49
Joseph Volosin <josephvolosin@gmail.com>
2022-10-19 16:01:21 -0400

    Finished writing stepThrough to update list/breadcrumbs, working stepBack

--- data/static/data/api.js
+++ data/static/data/api.js
@@ -3,6 +3,26 @@
  */
 
 const API_URL = "https://5puisoo9r8.execute-api.us-east-1.amazonaws.com";
+const GDC_LIST = `
+<button type="button" class="list-group-item list-group-item-action" onclick="stepThrough(this)">GDC-1</button>
+<button type="button" class="list-group-item list-group-item-action" onclick="stepThrough(this)">GDC-2</button>
+<button type="button" class="list-group-item list-group-item-action" onclick="stepThrough(this)">GDC-3</button>
+<button type="button" class="list-group-item list-group-item-action" onclick="stepThrough(this)">GDC-4</button>
+<button type="button" class="list-group-item list-group-item-action" onclick="stepThrough(this)">GDC-5</button>
+<button type="button" class="list-group-item list-group-item-action" onclick="stepThrough(this)">GDC-6</button>`
+const SUBTYPE_LIST = `
+    <button type="button" class="list-group-item list-group-item-action" onclick="stepThrough(this)">CAPE</button>
+    <button type="button" class="list-group-item list-group-item-action" onclick="stepThrough(this)">AETHER</button>
+    <button type="button" class="list-group-item list-group-item-action" onclick="stepThrough(this)">MoSAIC</button>
+    <button type="button" class="list-group-item list-group-item-action" onclick="stepThrough(this)">Ephem</button>`
+const DATA_LEVEL_LIST = `
+<button type="button" class="list-group-item list-group-item-action" onclick="stepThrough(this)">QL</button>
+<button type="button" class="list-group-item list-group-item-action" onclick="stepThrough(this)">L0</button>
+<button type="button" class="list-group-item list-group-item-action" onclick="stepThrough(this)">L1</button>
+<button type="button" class="list-group-item list-group-item-action" onclick="stepThrough(this)">L2</button>`
+const GDC_REGEX = /^GDC-\d$/;
+const SUBTYPE_REGEX = /^(CAPE|AETHER|MoSAIC|EPHEM)$/;
+const DATA_LEVEL_REGEX = /^(QL|L0|L1|L2|)$/;
 
 /**
  * Retrieves a presigned url for the given item using the API.
@@ -41,12 +61,37 @@ function getData() {
 }
 
 function stepThrough(currentElement) {
+    let wasContentUpdated = false;
     let currentElementText = currentElement.innerText;
-    let gdcRegex = /^GDC-\d$/;
-
-    if (currentElementText.match(gdcRegex)) {
-        // Replace the list elements with the subtype list
-        $(currentElement.parentElement).empty();
-        $(currentElement.parentElement).appendHTML(subtypeList);
+    let parentElement = $(currentElement.parentElement);
+    if (currentElementText.match(GDC_REGEX)) {
+        // Update the list group
+        parentElement.empty();
+        parentElement.append(SUBTYPE_LIST);
+        wasContentUpdated = true;
+        // Update the breadcrumb view
+    } else if (currentElementText.match(SUBTYPE_REGEX)) {
+        parentElement.empty();
+        parentElement.append(DATA_LEVEL_LIST);
+        wasContentUpdated = true;
+    } else if (currentElementText.match(DATA_LEVEL_REGEX)) {
+        parentElement.empty();
+        // Actuall start loading data from the API
+        wasContentUpdated = true;
     }
+
+    if (wasContentUpdated === true) {
+        let lastBreadcrumbText = $("#data-breadcrumbs").children().last()[0].innerHTML;
+        $("#data-breadcrumbs").children().last().removeClass('active');
+        $("#data-breadcrumbs").children().last().replaceWith(`<li class="breadcrumb-item"><a href="">` + lastBreadcrumbText + `</a></li>`);
+        $("#data-breadcrumbs").append(`<li class="breadcrumb-item active" aria-current="page">` + currentElementText + `</li>`);
+    }
+}
+
+function stepBack(currentElement) {
+    /** TODO */
+    let currentElementText = currentElement.innerText;
+    $("#data-breadcrumbs").children().last().remove();
+    $("#data-breadcrumbs").children().last().addClass("active");
+    $("#data-breadcrumbs").children().last().replaceWith(`<li class="breadcrumb-item">` + lastBreadcrumbText + `</li>`);
 }
