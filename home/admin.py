d26dda7a448c74dffe97c481abb95c0241e90713
Joseph Volosin <jvolosin@localhost.localdomain>
2022-11-28 13:29:28 -0500

    Removing old files, no longer needed after Wagtail upgrade

--- home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import SCUPBio, MoSAICBio, CAPEBio, AETHERBio
-admin.site.register(SCUPBio)
-admin.site.register(MoSAICBio)
-admin.site.register(CAPEBio)
-admin.site.register(AETHERBio)
