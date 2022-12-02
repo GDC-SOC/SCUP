4ae4285ab5b7d21d81321d64be39e7917818fdb8
Joseph Volosin <jvolosin@localhost.localdomain>
2022-12-02 14:11:21 -0500

    Removed media's .keep and added to ignore, added CSRF_TRUSTED_ORIGINS to fix invalid CSRF errors

--- SCUP/settings/base.py
+++ SCUP/settings/base.py
@@ -174,3 +174,5 @@ WAGTAILSEARCH_BACKENDS = {
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 WAGTAILADMIN_BASE_URL = "http://example.com"
+
+CSRF_TRUSTED_ORIGINS = SETTINGS_JSON_PARSED["CSRF_TRUSTED_ORIGINS"]
