a95397342a379c7fdbf781d972d331156b66b819
Volosin <jpvolosi@ndc.nasa.gov>
2024-05-10 12:42:05 -0400

    Adding package upgrade to pre_deploy script, adding codedeploy agent install to CloudFormation

--- deploy_scripts/pre_deploy.py
+++ deploy_scripts/pre_deploy.py
@@ -4,6 +4,7 @@
 
 import os
 import shutil
+import subprocess
 
 SCUP_INSTALL_PATH = "/opt/bitnami/projects/SCUP/"
 
@@ -17,6 +18,20 @@ def clear_install_path():
         shutil.rmtree(SCUP_INSTALL_PATH)
     except Exception as e:
         print(f"Failed to clear '{SCUP_INSTALL_PATH}' because: {str(e)}")
+
+def update_packages():
+    """Wrapper to run 'sudo apt-get update' and 'sudo apt-get upgrade
+    """
+    # sudo apt-get update
+    try:
+        subprocess.check_call(["sudo", "apt-get", "update"])
+    except Exception as e:
+        print(f"Failed to run 'apt-get update' because: {str(e)}")
+    # sudo apt-get upgrade
+    try:
+        subprocess.check_call(["sudo", "apt-get", "upgrade"])
+    except Exception as e:
+        print(f"Failed to run 'apt-get upgrade' because: {str(e)}")
 
 if __name__ == "__main__":
     clear_install_path()
